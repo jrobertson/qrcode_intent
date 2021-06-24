f820a4ebbd2b68693973540c51854e0dd1bd4cc1
James Robertson <james@jamesrobertson.me.uk>
2021-06-24 10:21:01 +0100

    feature: Added cryptocurrency intent and more.

--- lib/qrcode_intent.rb
+++ lib/qrcode_intent.rb
@@ -6,14 +6,66 @@ require 'rqrcode'
 require 'simplevpim'
 
 
+# # Intents covered
+# 
+# ## Cryptocurrency
+# 
+# * type
+# * receiver
+# * amount
+# * msg (optional)
+# 
+# ## Email
+# 
+# * subject
+# * body
+# 
+# ## Location
+# 
+# * longitude
+# * latitude
+# 
+# # Phone
+# 
+# * number
+# 
+# ## SMS
+# 
+# * number
+# * msg
+# 
+# ## Twitter
+# 
+# * username
+# * msg
+# 
+# ## WiFi
+# 
+# * ssid
+# * type; options: none, wep, wpa/wpa2
+# * password
+# 
+# ## Zoom
+# 
+# * meetingid
+# * password
+
+
 class QRCodeIntent
   
   attr_reader :to_s
-  
+    
+  Cryptocurrency = Struct.new(*%i(type receiver amount msg))
+  Email = Struct.new(*%i(to subject body))
+  Sms = Struct.new(*%i(number msg))
+  Twitter = Struct.new(*%i(username msg))
   WiFi = Struct.new(*%i(ssid type password))
+  Zoom = Struct.new(*%i(meetingid password))
   
   def initialize(obj=nil, event: nil, contact: nil, website: nil, 
-                 wifi: nil, location: nil, debug: false)
+                 wifi: nil, location: nil, sms: nil, phone: nil, 
+                 email: nil, bitcoin: nil, twitter: nil, 
+                 zoom: nil, debug: false)
     
     @debug = debug
     
@@ -24,6 +76,10 @@ class QRCodeIntent
     elsif location
       
       add_location location
+      
+    elsif email
+      
+      add_email email
       
     elsif event 
       
@@ -37,6 +93,26 @@ class QRCodeIntent
       
       add_wifi wifi
       
+    elsif sms
+      
+      add_sms sms
+      
+    elsif phone
+      
+      add_tel phone
+
+    elsif bitcoin
+      
+      add_cryptocurrency bitcoin            
+      
+    elsif twitter
+      
+      add_twitter twitter
+      
+    elsif zoom
+      
+      add_zoom zoom
+            
     else
       
       if obj.is_a? String then
@@ -61,7 +137,52 @@ class QRCodeIntent
       
     end
     
+  end
+  
+  def add_cryptocurrency(obj=nil, &blk)
+    
+    h = if block_given? then
+      
+      c = Cryptocurrency.new
+      yield c
+      c.to_h
+
+      
+    elsif obj.is_a? Hash
+      
+      obj
+      
+    end
+    
+    # a type can be bitcoin, bitcoincash, ethereum, litecoin, dash etc.
+    #
+    type = h[:type] || 'bitcoin'
+    
+    s = "%s:%s?%s" % [type, h[:receiver], h[:amount]]
+    s += "&message=%s" % h[:msg] if h[:msg]
+    
+    @to_s = s
+    
   end  
+
+  def add_email(obj=nil, &blk)
+    
+    h = if block_given? then
+      
+      email = Email.new
+      yield email
+      email.to_h
+
+      
+    elsif obj.is_a? Hash
+      
+      obj
+      
+    end
+    
+    @to_s = "MATMSG:TO:%s;SUB:%s;BODY:%s;;" % [h[:to], h[:subject], h[:body]]
+    
+  end    
   
   def add_event(obj=nil, &blk)
     
@@ -88,6 +209,75 @@ class QRCodeIntent
   def add_website(s)
     @to_s = s
   end
+  
+  def add_sms(obj=nil, &blk)
+    
+    h = if block_given? then
+      
+      sms = Sms.new
+      yield sms
+      sms.to_h
+
+      
+    elsif obj.is_a? Hash
+      
+      obj
+      
+    end
+    
+    @to_s = "SMSTO:%s:%s" % [h[:number], h[:msg]]
+    
+  end  
+  
+  # e.g. add_tel '0131 542 4923'
+  #
+  def add_tel(s=nil, number: s)
+    @to_s = 'tel:' + number
+  end  
+  
+  def add_twitter(obj=nil, &blk)
+    
+    h = if block_given? then
+      
+      c = Twitter.new
+      yield c
+      c.to_h
+
+      
+    elsif obj.is_a? Hash
+      
+      obj
+      
+    end
+    
+    @to_s = if h[:msg] then
+      "https://twitter.com/intent/tweet?text=" % URI.encode(h[:msg])
+    elsif h[:username]
+      "https://twitter.com/%s" % h[:username]
+    end
+    
+    @to_s = s
+    
+  end  
+  
+  def add_zoom(obj=nil, &blk)
+    
+    h = if block_given? then
+      
+      c = Zoom.new
+      yield c
+      c.to_h
+
+      
+    elsif obj.is_a? Hash
+      
+      obj
+      
+    end
+    
+    @to_s = "https://zoom.us/j/%s?pwd=%s" % [h[:meetingid], h[:password]]
+    
+  end  
   
   def add_wifi(obj=nil, &blk)
     
--- qrcode_intent.gemspec
+++ qrcode_intent.gemspec
@@ -1,8 +1,8 @@
 Gem::Specification.new do |s|
   s.name = 'qrcode_intent'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'An rqrcode wrapper for adding a website, a contact, ' + 
-      'wi-fi settings, location, or event.'
+      'wi-fi settings, location, event, cryptocurrency and more'
   s.authors = ['James Robertson']
   s.files = Dir['lib/qrcode_intent.rb']
   s.add_runtime_dependency('simplevpim', '~> 0.5', '>=0.5.2')
